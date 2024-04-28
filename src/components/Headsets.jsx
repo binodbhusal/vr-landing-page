c729c375ede77c99016f07d78e8e149316c7f771
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-04-28 20:50:41 +0100

    Add Headsets component

---
+++ src/components/Headsets.jsx
@@ -0,0 +1,59 @@
+import Headsets1 from '../assets/img/headset-1.png';
+import Headsets2 from '../assets/img/headset-2.png';
+import Headsets3 from '../assets/img/headset-3.png';
+import Headsets4 from '../assets/img/headset-4.png';
+
+const Headsets = () => (
+  <section className="py-12 lg:py-24">
+    <div className="container mx-auto">
+      <h2 className="font-bold mb-6 text-3xl">Mixed Reality Headsets</h2>
+      <div className="grid gap-y-4 lg:grid-cols-2 lg:gap-y-9">
+        <div className="relative">
+          <img src={Headsets1} alt="" />
+          <div className="absolute bottom-0 bg-white/20 backdrop-blur-md max-w-[300px] min-h-[150px] p-6">
+            <h4 className="font-semibold text-xl mb-2 ">Metaverse</h4>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Enim explicabo nihil cum distinctio adipisci obcaecati!
+            </p>
+          </div>
+
+        </div>
+        <div className="relative">
+          <img src={Headsets2} alt="" />
+          <div className="absolute bottom-0 bg-white/20 backdrop-blur-md max-w-[300px] min-h-[150px] p-6">
+            <h4 className="font-semibold text-xl mb-2 ">AIoT</h4>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Enim explicabo nihil cum distinctio adipisci obcaecati!
+            </p>
+          </div>
+
+        </div>
+        <div className="relative">
+          <img src={Headsets3} alt="" />
+          <div className="absolute bottom-0 bg-white/20 backdrop-blur-md max-w-[300px] min-h-[150px] p-6">
+            <h4 className="font-semibold text-xl mb-2 ">HoloLens</h4>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Enim explicabo nihil cum distinctio adipisci obcaecati!
+            </p>
+          </div>
+
+        </div>
+        <div className="relative">
+          <img src={Headsets4} alt="" />
+          <div className="absolute bottom-0 bg-white/20 backdrop-blur-md max-w-[300px] min-h-[150px] p-6">
+            <h4 className="font-semibold text-xl mb-2 ">TPCASTT</h4>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Enim explicabo nihil cum distinctio adipisci obcaecati!
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </section>
+);
+export default Headsets;
