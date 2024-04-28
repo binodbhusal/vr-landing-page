26665a186e7c5c71fab2a9b7d44829512c2168eb
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-04-28 23:26:06 +0100

    Add data.js

---
+++ src/data.js
@@ -0,0 +1,56 @@
+// import slider images
+import Avatar1 from './assets/img/avt1.png';
+import Avatar2 from './assets/img/avt2.png';
+import Avatar3 from './assets/img/avt3.png';
+
+const slider = [
+  {
+    id: 1,
+    message:
+      'I know in real-time where the money is spent,and I don’t have to lend out the company’s credit card anymore. What a relief!',
+    image: Avatar1,
+    name: 'Laurie Howell',
+    email: 'la.howell@gmail.com',
+  },
+  {
+    id: 2,
+    message:
+      'VISUALS are much better. The improvements in optics and resolution and much more than a modern console generation leap.',
+    image: Avatar2,
+    name: 'Christie Ernser',
+    email: 'chr.ernser@gmail.com',
+  },
+  {
+    id: 3,
+    message:
+      'I can actually see the improvement in the graphics, not having the external sensors is a big plus.',
+    image: Avatar3,
+    name: 'Frankie Collins',
+    email: 'frank.collins@gmail.com',
+  },
+  {
+    id: 4,
+    message:
+      'I know in real-time where the money is spent,and I don’t have to lend out the company’s credit card anymore. What a relief!',
+    image: Avatar1,
+    name: 'Laurie Howell',
+    email: 'la.howell@gmail.com',
+  },
+  {
+    id: 5,
+    message:
+      'VISUALS are much better. The improvements in optics and resolution and much more than a modern console generation leap.',
+    image: Avatar2,
+    name: 'Christie Ernser',
+    email: 'chr.ernser@gmail.com',
+  },
+  {
+    id: 6,
+    message:
+      'I can actually see the improvement in the graphics, not having the external sensors is a big plus.',
+    image: Avatar3,
+    name: 'Frankie Collins',
+    email: 'frank.collins@gmail.com',
+  },
+];
+export default slider;
